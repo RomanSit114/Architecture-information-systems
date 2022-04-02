367b4c7f076f51aa5fd6142d57c4d8fb2116ebcd
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2022-04-02 13:34:06 +0300

    Create prototype.cpp

---
+++ PZ1/prototype.cpp
@@ -0,0 +1,26 @@
+#include <opencv2\opencv.hpp>
+
+using namespace cv;
+
+using namespace std;
+
+int main() 
+{
+    Mat image;
+    namedWindow("Display window");
+    VideoCapture cap(0); // change if camera doesnt work
+
+    if (!cap.isOpened())
+    {
+        cout << "cannot open camera";
+    }
+
+    while (true)
+    {
+        cap >> image;
+        imshow("Display window", image);
+        waitKey(25);
+    }
+
+    return 0;
+}
